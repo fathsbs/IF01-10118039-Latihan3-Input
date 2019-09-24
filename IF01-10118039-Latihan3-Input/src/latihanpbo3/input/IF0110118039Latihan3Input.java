3a7fad71a8b61d9f1406660389d253e04addb7ed
Fatahillah Satria Bima Seno <55565303+fathsbs@users.noreply.github.com>
2019-09-24 17:51:19 +0700

    Update IF0110118039Latihan3Input.java

--- IF01-10118039-Latihan3-Input/src/latihanpbo3/input/IF0110118039Latihan3Input.java
+++ IF01-10118039-Latihan3-Input/src/latihanpbo3/input/IF0110118039Latihan3Input.java
@@ -2,7 +2,7 @@
  * NAMA              : FATAHILLAH SATRIA BIMA SENO
  * KELAS             : IF-01
  * NIM               : 10118039
- * DESKRIPSI PROGRAM :   
+ * DESKRIPSI PROGRAM : Program ini berisi program cara meng-inout data  
  */
 package latihanpbo3.input;
 
